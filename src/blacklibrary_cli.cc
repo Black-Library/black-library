b4fe50213007bad8ec7c51c9cb2a5ae2bbc2dcc5
Benjamin Chang <benjaminchang23@gmail.com>
2021-05-22 21:45:50 -0400

    feat: add cli driver

---
+++ src/blacklibrary_cli.cc
@@ -0,0 +1,95 @@
+/**
+ * blacklibrary_cli.cc
+ */
+
+#include <getopt.h>
+#include <signal.h>
+#include <string.h>
+
+#include <BlackLibraryCLI.h>
+
+struct options
+{
+    std::string db_path = "";
+    std::string storage_path = "";
+};
+
+black_library::BlackLibraryCLI *blacklibrary_cli;
+
+
+static void Usage(const char *prog)
+{
+    const char *p = strchr(prog, '/');
+    printf("usage: %s --path_(d)b --path_(s)torage [-h]\n", p ? (p + 1) : prog);
+}
+
+static int ParseOptions(int argc, char **argv, struct options *opts)
+{
+    static const char *const optstr = "d:hs:";
+    static const struct option long_opts[] = {
+        { "path_db", required_argument, 0, 'd' },
+        { "help", no_argument, 0, 'h' },
+        { "path_storage", required_argument, 0, 's' },
+        { 0, 0, 0, 0 }
+    };
+
+    if (!argv || !opts)
+        return -1;
+
+    int opt = 0;
+    while ((opt = getopt_long(argc, argv, optstr, long_opts, 0)) >= 0)
+    {
+        switch (opt)
+        {
+            case 'd':
+                opts->db_path = std::string(optarg);
+                break;
+            case 'h':
+                Usage(argv[0]);
+                exit(0);
+                break;
+            case 's':
+                opts->storage_path = std::string(optarg);
+                break;
+            default:
+                exit(1);
+                break;
+        }
+    }
+
+    if (optind < argc)
+    {
+        fprintf(stderr, "trailing options..\n");
+        exit(1);
+    }
+
+    return 0;
+}
+
+void SigHandler(int sig)
+{
+    if (sig == SIGTERM || sig == SIGINT)
+        blacklibrary_cli->Stop();
+}
+
+int main(int argc, char* argv[])
+{
+    struct options opts;
+
+    signal(SIGINT, SigHandler);
+    signal(SIGTERM, SigHandler);
+
+    if (ParseOptions(argc, argv, &opts))
+    {
+        Usage(argv[0]);
+        exit(1);
+    }
+
+    black_library::BlackLibraryCLI cli(opts.db_path, opts.storage_path);
+
+    blacklibrary_cli = &cli;
+
+    blacklibrary_cli->Run();
+
+    return 0;
+}
