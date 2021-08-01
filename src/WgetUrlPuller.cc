30ad2d3837548c6c8d7285c6973548c53ada1ee5
Benjamin Chang <benjaminchang23@gmail.com>
2021-08-01 19:38:15 -0400

    feat: doc_url split and add todo for internet con check

--- src/WgetUrlPuller.cc
+++ src/WgetUrlPuller.cc
@@ -21,8 +21,11 @@ std::vector<std::string> WgetUrlPuller::PullUrls() const
     std::stringstream ss;
     std::string file_line;
     const auto file_name = "black_library_urls";
+    const auto doc_url = "https://docs.google.com/document/d/1kSsAoUKg6aiXHb_sksAM5qpcCLYNS9n8-MfSgTg9XAY";
 
-    ss << "wget --quiet " << "https://docs.google.com/document/d/1kSsAoUKg6aiXHb_sksAM5qpcCLYNS9n8-MfSgTg9XAY" << "/export?format=txt --output-document ";
+    // TODO: check if connected to internet first
+
+    ss << "wget --quiet " << doc_url << "/export?format=txt --output-document ";
     ss << file_name;
 
     std::cout << "Pulling Urls using Wget" << std::endl;
