323132a12d4804ce8b5734a21902b888e06ff102
Benjamin Chang <benjaminchang23@gmail.com>
2021-08-28 22:34:16 -0400

    fix: actually use BLACKLIBRARY_FREQUENCY define

--- src/BlackLibrary.cc
+++ src/BlackLibrary.cc
@@ -46,7 +46,7 @@ int BlackLibrary::Run()
     {
         const auto deadline = std::chrono::steady_clock::now() + std::chrono::milliseconds(1000);
 
-        if (seconds_counter >= 21600)
+        if (seconds_counter >= BLACKLIBRARY_FREQUENCY)
             seconds_counter = 0;
 
         if (seconds_counter == 0)
