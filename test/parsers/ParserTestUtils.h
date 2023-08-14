f266ef3bb3497ffabce4251d35712feba7db1512
Benjamin Chang <benjaminchang23@gmail.com>
2023-08-14 17:46:02 -0400

    feat: start version check test

--- test/parsers/ParserTestUtils.h
+++ test/parsers/ParserTestUtils.h
@@ -6,6 +6,7 @@
 #define __BLACK_LIBRARY_CORE_PARSERS_PARSER_TEST_UTILS_H__
 
 #include <ConfigOperations.h>
+#include <VersionOperations.h>
 
 namespace black_library {
 
@@ -13,10 +14,35 @@ namespace core {
 
 namespace parsers {
 
+namespace BlackLibraryCommon = black_library::core::common;
+
 static constexpr const char DefaultTestLogPath[] = "/tmp/log";
 static constexpr const char DefaultTestStoragePath[] = "/tmp/store";
 
 static constexpr const char RR_DUMMY_URL[] = "https://www.royalroad.com/fiction/00000/some-fiction";
+
+static constexpr const char RR_DUMMY_UUID[] = "b0ad76bc-39d9-40ac-8de7-c5020568526d";
+
+static constexpr const char RR_DUMMY_CONTENT_0[] = "dummy content 0 to hash";
+static constexpr const char RR_DUMMY_CONTENT_1[] = "dummy content 1 to hash";
+static constexpr const char RR_DUMMY_CONTENT_2[] = "dummy content 2 to hash";
+static constexpr const char RR_DUMMY_CONTENT_3[] = "dummy content 3 to hash";
+static constexpr const char RR_DUMMY_CONTENT_4[] = "dummy content 4 to hash";
+static constexpr const char RR_DUMMY_CONTENT_5[] = "dummy content 5 to hash";
+
+static constexpr const char RR_MD5_0[] = "c353b81d9f3bcc779c05dcba168fb4d6";
+static constexpr const char RR_MD5_1[] = "ca24ea7ffe0e27b1f988bcb389660d9f";
+static constexpr const char RR_MD5_2[] = "53c5559beb9b465897ba1f1e4064985b";
+static constexpr const char RR_MD5_3[] = "20cf030687bea368437d453fd9f4a546";
+static constexpr const char RR_MD5_4[] = "20f190598790f44930a0b15af67e15e5";
+static constexpr const char RR_MD5_5[] = "762a218ffcca26773409886afcd25680";
+
+static constexpr const char RR_URL_0[] = "https://www.royalroad.com/fiction/00000/some-fiction/0";
+static constexpr const char RR_URL_1[] = "https://www.royalroad.com/fiction/00000/some-fiction/1";
+static constexpr const char RR_URL_2[] = "https://www.royalroad.com/fiction/00000/some-fiction/2";
+static constexpr const char RR_URL_3[] = "https://www.royalroad.com/fiction/00000/some-fiction/3";
+static constexpr const char RR_URL_4[] = "https://www.royalroad.com/fiction/00000/some-fiction/4";
+static constexpr const char RR_URL_5[] = "https://www.royalroad.com/fiction/00000/some-fiction/5";
 
 njson GenerateParserTestConfig()
 {
@@ -28,6 +54,72 @@ njson GenerateParserTestConfig()
     j["config"]["parser_debug_log"] = true;
 
     return j;
+}
+
+std::unordered_map<uint8_t, BlackLibraryCommon::Md5Sum> GenerateFullTestMd5Map()
+{
+    std::unordered_map<uint8_t, BlackLibraryCommon::Md5Sum> md5_map;
+
+    BlackLibraryCommon::Md5Sum md5_0;
+    BlackLibraryCommon::Md5Sum md5_1;
+    BlackLibraryCommon::Md5Sum md5_2;
+    BlackLibraryCommon::Md5Sum md5_3;
+    BlackLibraryCommon::Md5Sum md5_4;
+    BlackLibraryCommon::Md5Sum md5_5;
+
+    md5_0.uuid = RR_DUMMY_UUID;
+    md5_0.index_num = 0;
+    md5_0.md5_sum = RR_MD5_0;
+    md5_0.url = RR_URL_0;
+
+    md5_1.uuid = RR_DUMMY_UUID;
+    md5_1.index_num = 1;
+    md5_1.md5_sum = RR_MD5_1;
+    md5_1.url = RR_URL_1;
+
+    md5_2.uuid = RR_DUMMY_UUID;
+    md5_2.index_num = 2;
+    md5_2.md5_sum = RR_MD5_2;
+    md5_2.url = RR_URL_2;
+
+    md5_3.uuid = RR_DUMMY_UUID;
+    md5_3.index_num = 3;
+    md5_3.md5_sum = RR_MD5_3;
+    md5_3.url = RR_URL_3;
+
+    md5_4.uuid = RR_DUMMY_UUID;
+    md5_4.index_num = 4;
+    md5_4.md5_sum = RR_MD5_4;
+    md5_4.url = RR_URL_4;
+
+    md5_5.uuid = RR_DUMMY_UUID;
+    md5_5.index_num = 5;
+    md5_5.md5_sum = RR_MD5_5;
+    md5_5.url = RR_URL_5;
+
+    md5_map.emplace(0, md5_0);
+    md5_map.emplace(1, md5_1);
+    md5_map.emplace(2, md5_2);
+    md5_map.emplace(3, md5_3);
+    md5_map.emplace(4, md5_4);
+    md5_map.emplace(5, md5_5);
+
+    return md5_map;
+}
+
+size_t GenerateVersionOffset(std::vector<BlackLibraryCommon::Md5Sum> md5s)
+{
+    size_t offset = 0;
+    // get largest index_num if we need it later
+    for (auto const & md5 : md5s)
+    {
+        if (md5.second.index_num >= md5_index_num_offset_)
+        {
+            md5_index_num_offset_ = md5.second.index_num + 1;
+            last_update_date_ = md5.second.date;
+            last_url_ = md5.second.url;
+        }
+    }
 }
 
 } // namespace parsers
