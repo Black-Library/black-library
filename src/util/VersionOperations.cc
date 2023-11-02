482c401dcdad0db9c82fdef54f39f9e9974f331e
Benjamin Chang <benjaminchang23@gmail.com>
2023-11-02 00:35:07 -0400

    feat: update verison ops with new evp method

--- src/util/VersionOperations.cc
+++ src/util/VersionOperations.cc
@@ -5,7 +5,7 @@
 #include <iomanip>
 #include <sstream>
 
-#include <openssl/md5.h>
+#include <openssl/evp.h>
 
 #include <VersionOperations.h>
 
@@ -15,17 +15,37 @@ namespace core {
 
 namespace common {
 
-std::string GetMD5Hash(const std::string &input)
+std::string CalculateMd5Manual(unsigned char* buf, unsigned int buf_size)
 {
-    unsigned char result[MD5_DIGEST_LENGTH];
     std::ostringstream oss;
+    EVP_MD_CTX *mdctx;
+    unsigned char *md5_digest;
+    unsigned int md5_digest_len = EVP_MD_size(EVP_md5());
+    
+    // MD5_Init
+    mdctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(mdctx, EVP_md5(), NULL);
 
-    MD5((unsigned char*)input.c_str(), input.size(), result);
+    // MD5_Update
+    EVP_DigestUpdate(mdctx, buf, buf_size);
+
+    // MD5_Final
+    md5_digest = (unsigned char *)OPENSSL_malloc(md5_digest_len);
+    EVP_DigestFinal_ex(mdctx, md5_digest, &md5_digest_len);
+    EVP_MD_CTX_free(mdctx);
 
     oss << std::hex << std::setfill('0');
-    for (auto c : result) oss << std::setw(2) << (int)c;
+    for (unsigned int i = 0; i < md5_digest_len; ++i)
+    {
+        oss << std::setw(2) << (int)md5_digest[i];
+    }
 
     return oss.str();
+}
+
+std::string GetMD5Hash(const std::string &input)
+{
+    return CalculateMd5Manual((unsigned char*)input.c_str(), input.size());
 }
 
 } // namespace common
