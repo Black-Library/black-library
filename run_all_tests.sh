1b8d739b3bdb5005505822eb56894336bd82260f
Benjamin Chang <benjaminchang23@gmail.com>
2024-01-20 21:56:32 -0500

    feat: add script for running all tests, fix tests for md5 update

---
+++ run_all_tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+./install/test/config_test
+./install/test/db_test
+./install/test/file_test
+./install/test/index_entry_parser_test
+./install/test/log_test
+./install/test/parser_db_adapter_test
+./install/test/parser_factory_test
+./install/test/parser_manager_test
+./install/test/parser_test
+./install/test/parser_version_test
+./install/test/source_test
+./install/test/sqlite_db_test
+./install/test/string_test
+./install/test/time_test
+./install/test/version_test
