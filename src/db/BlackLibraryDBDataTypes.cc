4286971dcb929e62751cac748a6585e4907e035e
Benjamin Chang <benjaminchang23@gmail.com>
2024-03-18 21:57:05 -0400

    feat: fill out entry to json func

--- src/db/BlackLibraryDBDataTypes.cc
+++ src/db/BlackLibraryDBDataTypes.cc
@@ -14,7 +14,21 @@ void to_json(njson &j, const DBEntry &entry)
 {
     j = njson{
         { "UUID", entry.uuid },
-        { "title", entry.title }
+        { "title", entry.title },
+        { "author", entry.title },
+        { "nickname", entry.nickname },
+        { "source", entry.source },
+        { "url", entry.url },
+        { "last_url", entry.last_url },
+        { "series", entry.series },
+        { "series_length", entry.series_length },
+        { "version", entry.version },
+        { "media_path", entry.media_path },
+        { "birth_date", entry.birth_date },
+        { "check_date", entry.check_date },
+        { "update_date", entry.update_date },
+        { "user_contributed", entry.user_contributed },
+        { "processing", entry.processing },
     };
 }
 
